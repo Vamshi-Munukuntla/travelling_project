335c4a024db0ab3ce87cef5565b19eb956eb2ab9
Vamshi-Munukuntla <vamshi.kumar59@gmail.com>
2023-03-24 18:20:04 +0530

    Issue resolved

---
+++ demo.py
@@ -0,0 +1,22 @@
+from travelling.pipeline.pipeline import Pipeline
+from travelling.exception import TravelException
+from travelling.logger import logging
+from travelling.config.configuration import Configuration
+from travelling.components.data_ingestion import DataIngestion
+import os
+
+
+def main():
+    try:
+        # config_path = os.path.join("config", "config.yaml")
+        # pipeline = Pipeline(Configuration(config_file_path=config_path))
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
